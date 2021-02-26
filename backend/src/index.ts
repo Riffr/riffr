f98e671f6140ad99b3a4785f4468ba0bb1fe186e
Alistair <johnyob132@gmail.com>
2021-02-26 17:01:33 +0000

    Update backend exports.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,13 @@
 export * from './Server';
+
 export * from './modules/Chat';
 export * from './modules/Room';
-export * from './modules/Signalling';
+export *  from './modules/Signalling';
+
+import * as Mesh from './modules/signalling/Mesh';
+import * as Peer from './modules/signalling/Peer';
+
+export {
+    Mesh,
+    Peer
+};

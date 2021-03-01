9f2bfb5724b068a688d680e5d3fd4d6267cb5216
Alistair <johnyob132@gmail.com>
2021-03-01 15:33:33 +0000

    ChatClient Fix

--- frontend/src/connections/ChatClient.ts
+++ frontend/src/connections/ChatClient.ts
@@ -1,7 +1,7 @@
 import { Room, RoomEmitter } from "./Room";
 import { Socket } from "./Socket";
 
-import { Message, User, ChatEvent } from '@riffr/backend';
+import { Message, User, ChatEvent, chat } from '@riffr/backend';
 
 import EventEmitter from "events";
 import StrictEventEmitter from "strict-event-emitter-types"
@@ -21,13 +21,13 @@ class ChatClient extends (EventEmitter as ChatEmitter) {
     static async createRoom(socket: Socket, roomId: string, user: User) {
         const chatSocket = new Socket(`${socket.uri}/chat`);
         const room = await Room.createRoom<User>(chatSocket, roomId, user);
-        return new ChatClient(socket, user, room);
+        return new ChatClient(chatSocket, user, room);
     }
 
     static async joinRoom(socket: Socket, roomId: string, user: User) {
         const chatSocket = new Socket(`${socket.uri}/chat`);
         const room = await Room.joinRoom<User>(chatSocket, roomId, user);
-        return new ChatClient(socket, user, room);
+        return new ChatClient(chatSocket, user, room);
     }
 
     private constructor(socket: Socket, user: User, room: Room<User>) {
@@ -38,11 +38,15 @@ class ChatClient extends (EventEmitter as ChatEmitter) {
         this.room = room;
 
         this.socket.on(ChatEvent.Message, (message: Message) => {
+            console.log("ChatEvent Message");
             this.emit("message", this, message);
         })
     }
 
     public send(message: string) {
+        console.log("ChatClient send")
+        console.log(this.socket);
+        this.socket.emit("chat/message", "test");
         this.socket.emit(ChatEvent.Message, { from: this.user, content: message } as Message);
     }
 

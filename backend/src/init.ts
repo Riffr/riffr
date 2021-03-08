b8e6d396ebdc2f05d50789fc128c03b6587575f9
Alistair <johnyob132@gmail.com>
2021-03-08 17:54:53 +0000

    Update backend init script to use HTTPS

--- backend/src/init.ts
+++ backend/src/init.ts
@@ -6,17 +6,26 @@ import { signalling } from "./modules/Signalling";
 import { Server as IOServer } from "socket.io";
 
 const start = () => {
-    
-    const io = new IOServer(10000, {
+
+    const fs = require('fs');
+
+    const https = require("https");
+    const secureServer = https.createServer({
+            key: fs.readFileSync(".ssl/server.key"),
+            cert: fs.readFileSync(".ssl/server.cert")
+        });
+
+    const io = new IOServer(secureServer, {
         cors: {
             origin: "*"
         }
     });
+    io.listen(443);
     const server = new Server(io);
 
-    console.log(`Attaching signalling module...`);
+    console.log('Attaching signalling module...');
     server.attachModule(signalling);
-    console.log(`Attaching chat module...`);
+    console.log('Attaching chat module...');
     server.attachModule(chat);
 
     server.start();

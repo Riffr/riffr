6291562a27477b074105d46ebb4a8640b2aa2cfe
Alistair <johnyob132@gmail.com>
2021-03-08 04:39:45 +0000

    Implemented Riffr Component

--- frontend/src/Riffr.tsx
+++ frontend/src/Riffr.tsx
@@ -1,55 +1,27 @@
-import { UserProps } from "@riffr/backend/dist";
-import { Socket } from "dgram";
 import { RouteComponentProps } from "react-router";
-import { withResource } from "./WithResource";
-
-interface RiffrLocationState {
-    roomId: string,
-    username: string,
-    create: boolean
-};
-
-interface RiffrProps extends RouteComponentProps<{}, {}, RiffrLocationState> {
-    socket: Socket;
-};
+import { Route, Switch } from "react-router-dom";
+import Lobby from "./Lobby";
+import { withChatClient } from "./WithChatClient";
 
 
-const Loading = () => (
-    <p>"Loading..."</p>
-)
-
-const Riffr = (props: RiffrProps) => {
-    const { socket } = props;
-    const { roomId, username, create } = props.location.state;
-    const userProps : UserProps = { username };
+interface RiffrProps extends RouteComponentProps {}
 
 
-    const resource = chatClientResource(create, socket, roomId, userProps);
 
-    const 
+const Riffr = withChatClient<RiffrProps>(props => {
+    const { chatClient } = props; 
+    const { path } = props.match;
 
-    return (<WithResource 
-                resource={resource}
-                fallback={<Loading />} 
-                component={{props} => <WithChatClient {...props} />}
-            />);
+    console.log('Riffr Mounted');
 
-    // withChatClient(props => {
-    //     const path = props.match.path;
-    //     const chatClient = props.chatClient;
-    
-    //     return <Switch>
-    //         <Route 
-    //             path={`${path}/lobby`} 
-    //             render={(props) => <Lobby {...props} chatClient={chatClient}/>}
-    //         />
-    //         <Route 
-    //             path={`${path}/room`} 
-    //             render={(props) => <Lobby {...props} chatClient={chatClient}/>}
-    //         />
-    //     </Switch>
-    // })
-}
+    return (<Switch>
+        <Route 
+            path={`${path}/lobby`} 
+            render={(props) => <Lobby {...props} chatClient={chatClient}/>}
+        />
+    </Switch>);
+
+});
 
 export {
     Riffr

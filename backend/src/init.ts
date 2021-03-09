d9b8e62b49cfc553d5ea5b39dd41a9bc1f525bca
Alistair <johnyob132@gmail.com>
2021-03-09 14:56:41 +0000

    Update Infrastructure.

--- backend/src/init.ts
+++ backend/src/init.ts
@@ -5,22 +5,35 @@ import { signalling } from "./modules/Signalling";
 
 import { Server as IOServer } from "socket.io";
 
+// import { } from 'fs';
+// import { createServer } from 'http';
+
 const start = () => {
 
-    const fs = require('fs');
+    // const fs = require('fs');
 
-    const https = require("https");
-    const secureServer = https.createServer({
-            key: fs.readFileSync(".ssl/server.key"),
-            cert: fs.readFileSync(".ssl/server.cert")
-        });
+    // const https = require("https");
+    // const secureServer = https.createServer({
+    //         key: fs.readFileSync(".ssl/server.key"),
+    //         cert: fs.readFileSync(".ssl/server.cert")
+    //     });
 
-    const io = new IOServer(secureServer, {
+    // const httpServer = createServer((req, res) => {
+    //     if (req.method == "GET" && req.url == "/health") {
+    //         res.writeHead(200);
+    //         res.end("Hello World");
+    //     }
+
+    //     res.writeHead(200);
+    //     res.end()
+    // });
+    // httpServer.listen(9999);
+
+    const io = new IOServer(10000, {
         cors: {
             origin: "*"
         }
     });
-    io.listen(443);
     const server = new Server(io);
 
     console.log('Attaching signalling module...');

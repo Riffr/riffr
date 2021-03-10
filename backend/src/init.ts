9449200403ae82c4d95e64d405e0b2c57f6f086a
Karl Mose <karl.h.mose@gmail.com>
2021-03-10 15:59:55 +0000

    Revert "???"
    This reverts commit b15aa3b2

--- backend/src/init.ts
+++ backend/src/init.ts
@@ -5,30 +5,8 @@ import { signalling } from "./modules/Signalling";
 
 import { Server as IOServer } from "socket.io";
 
-// import { } from 'fs';
-// import { createServer } from 'http';
-
 const start = () => {
-
-    // const fs = require('fs');
-
-    // const https = require("https");
-    // const secureServer = https.createServer({
-    //         key: fs.readFileSync(".ssl/server.key"),
-    //         cert: fs.readFileSync(".ssl/server.cert")
-    //     });
-
-    // const httpServer = createServer((req, res) => {
-    //     if (req.method == "GET" && req.url == "/health") {
-    //         res.writeHead(200);
-    //         res.end("Hello World");
-    //     }
-
-    //     res.writeHead(200);
-    //     res.end()
-    // });
-    // httpServer.listen(9999);
-
+    
     const io = new IOServer(10000, {
         cors: {
             origin: "*"
@@ -36,9 +14,9 @@ const start = () => {
     });
     const server = new Server(io);
 
-    console.log('Attaching signalling module...');
+    console.log(`Attaching signalling module...`);
     server.attachModule(signalling);
-    console.log('Attaching chat module...');
+    console.log(`Attaching chat module...`);
     server.attachModule(chat);
 
     server.start();
